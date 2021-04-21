3526b17d85af9f3feb5d56ceef5e72d0a10647cb
jtellis1991 <joshuaellis1991@gmail.com>
2021-04-21 21:36:09 +0000

    update models for att_accessors and add annotate gem

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,14 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :bigint           not null, primary key
+#  fan_id     :bigint           not null
+#  photo_id   :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Like < ApplicationRecord
-  belongs_to :fan, class_name: "User"
+  belongs_to :fan, class_name: "User", counter_cache: true
   belongs_to :photo
 end
