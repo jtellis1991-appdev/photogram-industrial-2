4b50830e2fbf471cdaa306706384b8f1e237a242
jtellis1991 <joshuaellis1991@gmail.com>
2021-04-22 13:00:45 +0000

    added cdn assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  resources :photos
+  root "photos#index"
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :comments
+  resources :follow_requests
+  resources :likes
+  resources :photos
 end
